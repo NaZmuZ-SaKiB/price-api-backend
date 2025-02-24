53c94a5458f1130b33038df526e6af79015f8c7b
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-02-24 12:04:44 +0600

    feat: add token middleware to validate API key and check expiration

---
+++ src/app/middlewares/token.ts
@@ -0,0 +1,27 @@
+import httpStatus from 'http-status';
+import AppError from '../errors/AppError';
+import catchAsync from '../utils/catchAsync';
+import { Token } from '../modules/token/token.model';
+
+const token = catchAsync(async (req, res, next) => {
+  const token = req.query?.apiKey;
+
+  if (!token) {
+    throw new AppError(httpStatus.UNAUTHORIZED, 'Token not provided');
+  }
+
+  const isTokenExists = await Token.findOne({ token: token });
+
+  if (!isTokenExists) {
+    throw new AppError(httpStatus.UNAUTHORIZED, 'Invalid token');
+  }
+
+  const isExpired = new Date(isTokenExists.exp) < new Date();
+
+  if (isExpired) {
+    throw new AppError(httpStatus.UNAUTHORIZED, 'Token expired');
+  }
+
+  req.token = isTokenExists;
+  next();
+});
