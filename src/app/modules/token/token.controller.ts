91518dda401d37fbcbc8671658e715343bbe7bd2
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-02-24 11:49:18 +0600

    feat: add token controller with CRUD operations and expired token removal

---
+++ src/app/modules/token/token.controller.ts
@@ -0,0 +1,67 @@
+import httpStatus from 'http-status';
+import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
+import { TokenService } from './token.service';
+
+const create = catchAsync(async (req, res) => {
+  const result = await TokenService.create(req.body);
+
+  sendResponse(res, {
+    statusCode: httpStatus.CREATED,
+    success: true,
+    message: 'Token created successfully',
+    data: result,
+  });
+});
+
+const get = catchAsync(async (req, res) => {
+  const result = await TokenService.get(req.params.id);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Token fetched successfully',
+    data: result,
+  });
+});
+
+const getAll = catchAsync(async (req, res) => {
+  const result = await TokenService.getAll();
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Tokens fetched successfully',
+    data: result,
+  });
+});
+
+const remove = catchAsync(async (req, res) => {
+  const result = await TokenService.remove(req.params.id);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Token removed successfully',
+    data: result,
+  });
+});
+
+const removeExpiredTokens = catchAsync(async (req, res) => {
+  const result = await TokenService.removeExpiredTokens();
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Expired tokens removed successfully',
+    data: result,
+  });
+});
+
+export const TokenController = {
+  create,
+  get,
+  getAll,
+  remove,
+  removeExpiredTokens,
+};
