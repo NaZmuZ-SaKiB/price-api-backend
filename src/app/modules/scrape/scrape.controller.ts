d0925362b585900a2dd33067eda2390a6cb1c4cc
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-02-25 20:01:46 +0600

    feat: add scrape controller to handle web scraping requests

---
+++ src/app/modules/scrape/scrape.controller.ts
@@ -0,0 +1,19 @@
+import httpStatus from 'http-status';
+import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
+import { ScrapeService } from './scrape.service';
+
+const scrape = catchAsync(async (req, res) => {
+  const result = await ScrapeService.scrape(req.body?.url);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Scraped successfully',
+    data: result,
+  });
+});
+
+export const ScrapeController = {
+  scrape,
+};
