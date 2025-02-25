2757f5a794b2101a34953e521aee023b78280577
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-02-25 20:02:10 +0600

    feat: enhance product model with additional fields for tracking and status

--- src/app/modules/product/product.model.ts
+++ src/app/modules/product/product.model.ts
@@ -1,23 +1,32 @@
-import { model, Schema } from "mongoose";
-import { TProduct } from "./product.type";
+import { model, Schema } from 'mongoose';
+import { TProduct } from './product.type';
 
-const productSchema = new Schema<TProduct>({
+const productSchema = new Schema<TProduct>(
+  {
     title: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     price: {
-        type: Number,
-        required: true,
+      type: Number,
+      required: true,
     },
     url: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
+    },
+    done: {
+      type: Boolean,
+      default: false,
     },
     image: String,
     status: String,
-},{
+    lastChecked: Date,
+    lastModified: Date,
+  },
+  {
     timestamps: true,
-})
+  },
+);
 
 export const Product = model<TProduct>('Product', productSchema);
