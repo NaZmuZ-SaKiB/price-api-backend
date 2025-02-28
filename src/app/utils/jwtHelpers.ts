84c465e3d48f181cedcbd49fc558c87f5d198c2e
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-02-28 20:36:19 +0600

    feat: add JWT helper functions for token generation and verification

---
+++ src/app/utils/jwtHelpers.ts
@@ -0,0 +1,30 @@
+import { SignJWT, jwtVerify } from 'jose';
+import { TTokenAccess } from '../modules/token/token.type';
+
+const generateToken = async (
+  payload: {
+    token: string;
+    access: TTokenAccess;
+  },
+  secret: string,
+  expiresIn: string,
+) => {
+  const encodedSecret = new TextEncoder().encode(secret);
+
+  const token = await new SignJWT(payload)
+    .setProtectedHeader({
+      alg: 'HS256',
+    })
+    .setExpirationTime(expiresIn)
+    .sign(encodedSecret);
+
+  return token;
+};
+
+const verifyToken = async (token: string, secret: string) => {
+  const encodedSecret = new TextEncoder().encode(secret);
+
+  return await jwtVerify(token, encodedSecret, { algorithms: ['HS256'] });
+};
+
+export const jwtHelpers = { generateToken, verifyToken };
