f72c4858faf5fdf1093f236deed246397bd87621
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-02-28 20:36:13 +0600

    feat: add JWT configuration variables to the environment settings

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -1,13 +1,16 @@
-import dotenv from "dotenv";
-import path from "path";
+import dotenv from 'dotenv';
+import path from 'path';
 
-dotenv.config({ path: path.join(process.cwd(), ".env") });
+dotenv.config({ path: path.join(process.cwd(), '.env') });
 
 export default {
   node_env: process.env.NODE_ENV,
   port: process.env.PORT,
 
   front_end_url: process.env.FRONT_END_URL,
+
+  jwt_access_secret: process.env.JWT_ACCESS_SECRET,
+  jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN,
 
   database_url: process.env.DATABASE_URL,
 };
