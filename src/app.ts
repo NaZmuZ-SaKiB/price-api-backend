86fb5f57555951da5561b366141f89732fe23bf7
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-03-01 16:28:50 +0600

    feat: add token middleware to protect API routes

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Application } from 'express';
 import cors from 'cors';
 import MainRouter from './app/routes';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import token from './app/middlewares/token';
 
 const app: Application = express();
 
@@ -10,7 +11,7 @@ app.use(express.json());
 app.use(cors({ origin: true, credentials: true }));
 
 // Application Routes
-app.use('/api', MainRouter);
+app.use('/api', token, MainRouter);
 
 app.get('/', (_, res) => {
   res.send('Price Api Backend!');
