6fff0cb10e6fee72b7d29207d630ebae5b507479
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-03-04 16:17:32 +0600

    feat: add admin access control to token routes for enhanced security

--- src/app/modules/token/token.route.ts
+++ src/app/modules/token/token.route.ts
@@ -1,20 +1,26 @@
 import { Router } from 'express';
 import { TokenController } from './token.controller';
 import token from '../../middlewares/token';
+import access from '../../middlewares/access';
 
 const router = Router();
 
 // GET
-router.get('/', token, TokenController.getAll);
+router.get('/', token, access('admin'), TokenController.getAll);
 router.get('/dashboard', token, TokenController.dashboard);
-router.get('/:id', token, TokenController.get);
+router.get('/:id', token, access('admin'), TokenController.get);
 
 // POST
-router.post('/', token, TokenController.create);
+router.post('/', token, access('admin'), TokenController.create);
 router.post('/sign-in', TokenController.signIn);
 
 // DELETE
-router.delete('/expired', token, TokenController.removeExpiredTokens);
-router.delete('/:id', token, TokenController.remove);
+router.delete(
+  '/expired',
+  token,
+  access('admin'),
+  TokenController.removeExpiredTokens,
+);
+router.delete('/:id', token, access('admin'), TokenController.remove);
 
 export const TokenRouter = router;
