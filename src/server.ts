67640f998726982f54fe0f78d9af6f69cecd5947
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-03-08 13:13:12 +0600

    feat: implement token creation for admin access if not present

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,23 @@ import mongoose from 'mongoose';
 
 import config from './app/config';
 import app from './app';
+import { Token } from './app/modules/token/token.model';
+import { TOKEN_ACCESS } from './app/modules/token/token.constant';
 
 const main = async () => {
   try {
     await mongoose.connect(config.database_url as string);
 
-    
+    const token = Token.findOne({ access: TOKEN_ACCESS.ADMIN });
+
+    if (!token) {
+      await Token.create({
+        access: TOKEN_ACCESS.ADMIN,
+        token: config.admin_password,
+        name: 'Admin',
+        exp: new Date(),
+      });
+    }
 
     app.listen(config.port, () => {
       console.log(`🚀 Application Running on PORT : ${config.port}`);
